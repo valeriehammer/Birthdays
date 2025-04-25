6b729eb0d1d9a4d49f205e0ddba992d78d5c8be4
Valerie Hammer <vkellogg@wisc.edu>
2025-04-25 08:59:06 -0600

    Initial Commit

---
+++ Birthdays/BirthdaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdaysApp.swift
+//  Birthdays
+//
+//  Created by Valerie Hammer on 4/25/25.
+//
+
+import SwiftUI
+
+@main
+struct BirthdaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
