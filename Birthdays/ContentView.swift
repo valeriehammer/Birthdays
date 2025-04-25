3257504b346ba3823b1e0c630a038a71c933beaf
Valerie Hammer <vkellogg@wisc.edu>
2025-04-25 09:48:23 -0600

    Complete, Create, and read operations with UI

--- Birthdays/ContentView.swift
+++ Birthdays/ContentView.swift
@@ -8,17 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var friends: [Friend] = [
+        Friend(name: "Taylor Swift", birthday: .now),
+        Friend(name: "Selena Gomez", birthday: Date(timeIntervalSince1970: 0))
+    ]
+    
+    @State private var newName = ""
+    @State private var newBirthday = Date.now
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List(friends, id: \.name) { Friend in
+                HStack{
+                    Text(Friend.name)
+                    Spacer()
+                    Text(Friend.birthday, format: .dateTime.month(.wide).day().year())
+                }
+                //  end HStack
+            }
+            // endList
+            .navigationTitle("Birthdays")
+            .safeAreaInset(edge: .bottom) {
+                VStack{
+                    Text("New Birthday")
+                        .font(.headline)
+                    DatePicker(selection: $newBirthday, in: Date.distantPast...Date.now, displayedComponents: .date) {
+                        TextField("Name", text: $newName)
+                            .textFieldStyle(.roundedBorder)
+                    }
+                    Button("Save"){
+                        let newFriend = Friend(name: newName, birthday: newBirthday)
+                        friends.append(newFriend)
+                        newName = ""
+                        newBirthday = Date.now
+                        
+                    }
+                    .bold()
+                }
+                .padding()
+                .background(.bar)
+            }
+            //
         }
-        .padding()
+        //    endNavigationStack
     }
+    //    endbody
 }
+//end struct
+    #Preview {
+        ContentView()
+    }
 
-#Preview {
-    ContentView()
-}
---
+++ Birthdays/Friend.swift
@@ -0,0 +1,18 @@
+//
+//  Friend.swift
+//  Birthdays
+//
+//  Created by Valerie Hammer on 4/25/25.
+//
+
+import Foundation
+
+class Friend {
+    var name: String
+    var birthday: Date
+    
+    init(name: String, birthday: Date) {
+        self.name = name
+        self.birthday = birthday
+    }
+}
